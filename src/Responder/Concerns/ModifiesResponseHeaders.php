4eff6191a4a11a2e78667f301008afe7e51bee34
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-08-01 17:45:06 -0700

    Add headers response trait

---
+++ src/Responder/Concerns/ModifiesResponseHeaders.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ToyWpRouting\Responder;
+
+// @todo status?
+trait ModifiesResponseHeaders
+{
+    protected array $modifiesResponseHeadersData = [
+        'headers' => [],
+    ];
+
+    public function withHeader(string $key, string $value): self
+    {
+        $this->modifiesResponseHeadersData['headers'][$key] = $value;
+
+        return $this;
+    }
+
+    public function withHeaders(array $headers): self
+    {
+        $this->modifiesResponseHeadersData['headers'] = [];
+
+        foreach ($headers as $key => $value) {
+            $this->withHeader($key, $value);
+        }
+
+        return $this;
+    }
+
+    protected function initializeModifiesResponseHeaders(): void
+    {
+        $this->addFilter('wp_headers', function ($headers) {
+            // @todo Handle multiple headers with same key?
+            // @todo Return our array of headers if ! is_array($headers)?
+            if (is_array($headers) && ! empty($this->modifiesResponseHeadersData['headers'])) {
+                $headers = array_merge($headers, $this->modifiesResponseHeadersData['headers']);
+            }
+
+            return $headers;
+        });
+    }
+}
