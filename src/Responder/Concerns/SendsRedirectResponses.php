877d989a944e4ca983bf978bca6a81329709b692
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-08-01 17:44:06 -0700

    Add redirect response trait

---
+++ src/Responder/Concerns/SendsRedirectResponses.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ToyWpRouting\Responder;
+
+use InvalidArgumentException;
+
+// @todo Conflicts - headers contain x-redirect-by, status,
+trait SendsRedirectResponses
+{
+    protected array $sendsRedirectResponsesData = [
+        'location' => '',
+        'redirectBy' => 'WordPress',
+        'safe' => true,
+        'status' => 302,
+    ];
+
+    public function withRedirectByHeader(string $redirectBy): self
+    {
+        $this->sendsRedirectResponsesData['redirectBy'] = $redirectBy;
+
+        return $this;
+    }
+
+    public function withRedirectLocation(string $location): self
+    {
+        $this->sendsRedirectResponsesData['location'] = $location;
+
+        return $this;
+    }
+
+    public function withRedirectStatusCode(int $statusCode): self
+    {
+        if (! ($statusCode >= 300 && $statusCode < 400)) {
+            throw new InvalidArgumentException('@todo');
+        }
+
+        $this->sendsRedirectResponsesData['status'] = $statusCode;
+
+        return $this;
+    }
+
+    public function withUnsafeRedirectsAllowed(): self
+    {
+        $this->sendsRedirectResponsesData['safe'] = false;
+
+        return $this;
+    }
+
+    protected function initializeSendsRedirectResponses()
+    {
+        $this->addAction('template_redirect', function () {
+            if (! $this->isSendingRedirectResponse()) {
+                return;
+            }
+
+            if ($this->sendsRedirectResponsesData['safe']) {
+                $cancelled = wp_safe_redirect(
+                    $this->sendsRedirectResponsesData['location'],
+                    $this->sendsRedirectResponsesData['status'],
+                    $this->sendsRedirectResponsesData['redirectBy']
+                );
+            } else {
+                $cancelled = wp_redirect(
+                    $this->sendsRedirectResponsesData['location'],
+                    $this->sendsRedirectResponsesData['status'],
+                    $this->sendsRedirectResponsesData['redirectBy']
+                );
+            }
+
+            if (! $cancelled) {
+                exit;
+            }
+        });
+
+        $this->addConflictCheck(function () {
+            if (! $this->isSendingRedirectResponse()) {
+                return;
+            }
+
+            if (
+                method_exists($this, 'isModifyingResponseHtmlTemplate')
+                && $this->isModifyingResponseHtmlTemplate()
+            ) {
+                return '@todo';
+            }
+
+            if (method_exists($this, 'isSendingJsonResponse') && $this->isSendingJsonResponse()) {
+                return '@todo';
+            }
+        });
+    }
+
+    protected function isSendingRedirectResponse(): bool
+    {
+        return is_string($this->sendsRedirectResponsesData['location'])
+            && '' !== $this->sendsRedirectResponsesData['location'];
+    }
+}
