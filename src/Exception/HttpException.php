392e7167dbf7f5549ad118fcd40b54c2fc987a40
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-08-19 17:36:21 -0700

    Move base response logic to HttpException class

--- src/Exception/HttpException.php
+++ src/Exception/HttpException.php
@@ -5,10 +5,35 @@ declare(strict_types=1);
 namespace ToyWpRouting\Exception;
 
 use RuntimeException;
+use Throwable;
 use ToyWpRouting\Responder\HttpExceptionResponder;
 
-// @todo Shouldn't be abstract?
-abstract class HttpException extends RuntimeException implements HttpExceptionInterface
+class HttpException extends RuntimeException implements HttpExceptionInterface
 {
-    abstract public function prepareResponse(HttpExceptionResponder $responder): void;
+    protected array $headers;
+    protected int $statusCode;
+
+    public function __construct(
+        int $statusCode,
+        string $message = '',
+        ?Throwable $previous = null,
+        array $headers = [],
+        int $code = 0
+    ) {
+        $this->statusCode = $statusCode;
+        $this->headers = $headers;
+
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function prepareResponse(HttpExceptionResponder $responder): void
+    {
+        $responder
+            ->withStatusCode($this->statusCode)
+            ->withHeaders($this->headers);
+
+        if (method_exists($this, 'doPrepareResponse')) {
+            $this->doPrepareResponse($responder);
+        }
+    }
 }
--- src/Exception/MethodNotAllowedHttpException.php
+++ src/Exception/MethodNotAllowedHttpException.php
@@ -4,27 +4,22 @@ declare(strict_types=1);
 
 namespace ToyWpRouting\Exception;
 
+use Throwable;
 use ToyWpRouting\Responder\HttpExceptionResponder;
 use WP_Query;
 
 class MethodNotAllowedHttpException extends HttpException
 {
-    protected array $allowedMethods;
+    public function __construct(
+        array $allowedMethods,
+        string $message = '',
+        ?Throwable $previous = null,
+        int $code = 0,
+        array $headers = []
+    ) {
+        $headers['Allow'] = strtoupper(implode(', ', $allowedMethods));
 
-    public function __construct(array $allowedMethods)
-    {
-        $this->allowedMethods = $allowedMethods;
-    }
-
-    public function prepareResponse(HttpExceptionResponder $responder): void
-    {
-        $responder->withBodyClass('error405');
-        $responder->withTitle('Method not allowed');
-        $responder->withHeader('Allow', strtoupper(implode(', ', $this->allowedMethods)));
-
-        // @todo Can existing responder traits be adapted to handle these?
-        $responder->withAction('parse_query', [$this, 'onParseQuery']);
-        $responder->withFilter('template_include', [$this, 'onTemplateInclude']);
+        parent::__construct(405, $message, $previous, $headers, $code);
     }
 
     /**
@@ -40,15 +35,17 @@ class MethodNotAllowedHttpException extends HttpException
         }
     }
 
+    public function onSendHeaders(): void
+    {
+        nocache_headers();
+    }
+
     public function onTemplateInclude(): string
     {
         // Alternatively we can allow wordpress to handle status header for us by setting the
         // 'error' query variable on the global wp instance to '405' within the 'parse_request'
         // action. However, the status header will generally be set back to 200 in $wp->handle_404()
         // so we would also need to filter 'pre_handle_404' to prevent this.
-        status_header(405);
-        nocache_headers();
-
         $errorTemplate = get_query_template('405');
 
         // Alternatively we might want to just fall back to the theme index template...
@@ -57,5 +54,18 @@ class MethodNotAllowedHttpException extends HttpException
         }
 
         return $errorTemplate;
+    }
+
+    protected function doPrepareResponse(HttpExceptionResponder $responder): void
+    {
+        $responder
+            ->withBodyClass('error405')
+            ->withTitle('Method not allowed');
+
+        // @todo Can existing responder traits be adapted to handle these?
+        $responder
+            ->withAction('parse_query', [$this, 'onParseQuery'])
+            ->withAction('send_headers', [$this, 'onSendHeaders'])
+            ->withFilter('template_include', [$this, 'onTemplateInclude']);
     }
 }
