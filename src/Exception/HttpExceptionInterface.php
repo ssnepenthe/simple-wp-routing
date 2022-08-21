aba8a829dfa1c6bc36ed2c4ae894aeed6d30828d
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-08-21 00:09:25 -0700

    Http exceptions have to be throwable

--- src/Exception/HttpExceptionInterface.php
+++ src/Exception/HttpExceptionInterface.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace ToyWpRouting\Exception;
 
+use Throwable;
 use ToyWpRouting\Responder\HttpExceptionResponder;
 
-interface HttpExceptionInterface
+interface HttpExceptionInterface extends Throwable
 {
     public function prepareResponse(HttpExceptionResponder $responder): void;
 }
