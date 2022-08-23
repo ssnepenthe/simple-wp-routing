d7f3a974e13cd388ca38385bf5662542a2202e68
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-08-23 13:31:03 -0700

    Simplify bundled 405 template
    Makes it easier to avoid problems with FSE themes
    
    Still overridable in themes by creating 405.php template

--- src/Exception/MethodNotAllowedHttpException.php
+++ src/Exception/MethodNotAllowedHttpException.php
@@ -6,7 +6,6 @@ namespace ToyWpRouting\Exception;
 
 use Throwable;
 use ToyWpRouting\Responder\HttpExceptionResponder;
-use WP_Query;
 
 class MethodNotAllowedHttpException extends HttpException
 {
@@ -24,13 +23,8 @@ class MethodNotAllowedHttpException extends HttpException
 
     public function onTemplateInclude(): string
     {
-        // Alternatively we can allow wordpress to handle status header for us by setting the
-        // 'error' query variable on the global wp instance to '405' within the 'parse_request'
-        // action. However, the status header will generally be set back to 200 in $wp->handle_404()
-        // so we would also need to filter 'pre_handle_404' to prevent this.
         $errorTemplate = get_query_template('405');
 
-        // Alternatively we might want to just fall back to the theme index template...
         if (! \is_string($errorTemplate) || '' === $errorTemplate) {
             $errorTemplate = realpath(__DIR__ . '/../../templates/405.php');
         }
--- templates/405.php
+++ templates/405.php
@@ -1,19 +1,59 @@
-<?php get_header(); ?>
+<?php
+/**
+ * Shamelessly (and poorly) adapted from the twentytwentytwo 404 template.
+ *
+ * @todo twentytwentytwo uses Source Serif Pro font family for h2 - worth bundling?
+ */
+?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="robots" content="noindex,nofollow,noarchive" />
+		<title>An Error Occurred: Method Not Allowed</title>
+		<style>
+			body {
+				background-color: #FFF;
+				color: #000;
+				font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+				font-size: 1.125rem;
+				line-height: 1.6;
+			}
 
-	<header>
-		<h1>
-			Oops! An Error Occurred
-		</h1>
+			div {
+				padding-left: max(1.25rem, 5vw);
+				padding-right: max(1.25rem, 5vw);
+			}
 
-		<h2>
-			The server returned a "405 Method Not Allowed".
-		</h2>
-	</header>
+			main {
+				margin-top: 1.5rem;
+			}
 
-	<div>
-		<p>
-			Something went wrong and we are working to fix it as quickly as possible. Sorry for the inconvenience.
-		</p>
-	</div>
+			h2 {
+				font-size: clamp(4rem, 40vw, 20rem);
+				font-weight: 200;
+				line-height: 1;
+				margin-bottom: 0;
+				margin-top: 0;
+				text-align: center;
+			}
 
-<?php get_footer(); ?>
+			p {
+				text-align: center;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div>
+			<main>
+				<h2>
+					405
+				</h2>
+
+				<p>
+					The page was requested with an unsupported HTTP method.
+				</p>
+			</main>
+		</div>
+	</body>
+</html>
