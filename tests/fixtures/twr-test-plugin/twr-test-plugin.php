2c65a403518350016774bd3872aa33cc1806b58a
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-09-12 13:55:15 -0700

    Merge test plugin test data into single callback

--- tests/fixtures/twr-test-plugin/twr-test-plugin.php
+++ tests/fixtures/twr-test-plugin/twr-test-plugin.php
@@ -21,22 +21,17 @@ use ToyWpRouting\RouteConverter;
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
 add_action('wp_footer', function () {
+    global $wp;
+
     echo '<div class="twr-test-data">';
+
+    printf('<span class="twr-rewrites">%s</span>', json_encode(get_option('rewrite_rules')));
+    printf('<span class="twr-query-vars">%s</span>', json_encode($wp->public_query_vars));
 
     do_action('twr_test_data');
 
     echo '</div>';
 }, 999);
-
-add_action('twr_test_data', function () {
-    printf('<span class="twr-rewrites">%s</span>', json_encode(get_option('rewrite_rules')));
-});
-
-add_action('twr_test_data', function () {
-    global $wp;
-
-    printf('<span class="twr-query-vars">%s</span>', json_encode($wp->public_query_vars));
-});
 
 (function () {
     // HTTP method tests.
