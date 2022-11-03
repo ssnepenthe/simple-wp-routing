206c0d6859b5296b7cb48fca86cf76ac780d3af9
ssnepenthe <ssnepenthe@users.noreply.github.com>
2022-11-03 12:35:31 -0700

    Pre-compute qvs and rules in compiled collection

--- tests/fixtures/rewrite-cache.php
+++ tests/fixtures/rewrite-cache.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-return function (?\ToyWpRouting\InvocationStrategyInterface $invocationStrategy = null) {
+return function (?\ToyWpRouting\InvocationStrategyInterface $invocationStrategy = null): \ToyWpRouting\RewriteCollection {
     return new class($invocationStrategy) extends \ToyWpRouting\RewriteCollection
     {
         protected bool $locked = true;
@@ -11,7 +11,16 @@ return function (?\ToyWpRouting\InvocationStrategyInterface $invocationStrategy 
         {
             parent::__construct('', $invocationStrategy);
 
-            $this->rewrites->attach(new \ToyWpRouting\Compiler\OptimizedRewrite(array (
+            $this->queryVariables = array (
+  'var' => 'var',
+  'matchedRule' => 'matchedRule',
+);
+            $this->rewriteRules = array (
+  '^first$' => 'index.php?var=first&matchedRule=9f79cebcf1735d5eaefeee8dbc7316dd',
+  '^second$' => 'index.php?var=second&matchedRule=3cf5d427e03a68a3881d2d68a86b64f1',
+);
+
+            $rewrite0 = new \ToyWpRouting\Compiler\OptimizedRewrite(array (
   0 => 'GET',
   1 => 'HEAD',
 ), array (
@@ -22,8 +31,8 @@ return function (?\ToyWpRouting\InvocationStrategyInterface $invocationStrategy 
 ), array (
   'var' => 'var',
   'matchedRule' => 'matchedRule',
-), $this->invocationStrategy, 'firsthandler', NULL));
-$this->rewrites->attach(new \ToyWpRouting\Compiler\OptimizedRewrite(array (
+), $this->invocationStrategy, 'firsthandler', NULL);
+$rewrite1 = new \ToyWpRouting\Compiler\OptimizedRewrite(array (
   0 => 'POST',
 ), array (
   0 => new \ToyWpRouting\Compiler\OptimizedRewriteRule('3cf5d427e03a68a3881d2d68a86b64f1', 'index.php?var=second&matchedRule=3cf5d427e03a68a3881d2d68a86b64f1', array (
@@ -33,7 +42,20 @@ $this->rewrites->attach(new \ToyWpRouting\Compiler\OptimizedRewrite(array (
 ), array (
   'var' => 'var',
   'matchedRule' => 'matchedRule',
-), $this->invocationStrategy, 'secondhandler', 'secondisactivecallback'));
+), $this->invocationStrategy, 'secondhandler', 'secondisactivecallback');
+$this->rewrites->attach($rewrite0);
+$this->rewrites->attach($rewrite1);
+$this->rewritesByHashAndMethod = array (
+  '9f79cebcf1735d5eaefeee8dbc7316dd' => 
+  array (
+    'GET' => $rewrite0,
+    'HEAD' => $rewrite0,
+  ),
+  '3cf5d427e03a68a3881d2d68a86b64f1' => 
+  array (
+    'POST' => $rewrite1,
+  ),
+);
         }
     };
 };
