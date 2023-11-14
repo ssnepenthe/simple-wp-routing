20a257abaa1190268de3e398bfc1db45ce6c7c40
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-11-14 15:11:58 -0700

    Add default callable resolver tests

---
+++ tests/Unit/CallableResolver/DefaultCallableResolverTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ToyWpRouting\Tests\Unit\CallableResolver;
+
+use PHPUnit\Framework\TestCase;
+use ToyWpRouting\CallableResolver\DefaultCallableResolver;
+use ToyWpRouting\Exception\BadCallableException;
+
+class DefaultCallableResolverTest extends TestCase
+{
+    /** @dataProvider providesTestResolveCallable */
+    public function testResolveCallable($callable)
+    {
+        $resolver = new DefaultCallableResolver();
+
+        $this->assertSame($callable, $resolver->resolve($callable));
+    }
+
+    public function testResolveThrowsForNonCallables()
+    {
+        $this->expectException(BadCallableException::class);
+        $this->expectExceptionMessage('is not callable');
+
+        $resolver = new DefaultCallableResolver();
+        $resolver->resolve('notCallableString');
+    }
+
+    public static function providesTestResolveCallable()
+    {
+        yield [__NAMESPACE__ . '\\defaultTestFunc'];
+        yield [fn () => ''];
+        yield [new DefaultInvokable()];
+        yield [DefaultInvokable::class . '::staticTestMethod'];
+        yield [[DefaultInvokable::class, 'staticTestMethod']];
+        yield [[new DefaultInvokable(), 'nonStaticTestMethod']];
+    }
+}
+
+function defaultTestFunc() {}
+
+class DefaultInvokable {
+    public function __invoke() {}
+    public static function staticTestMethod() {}
+    public function nonStaticTestMethod() {}
+}
