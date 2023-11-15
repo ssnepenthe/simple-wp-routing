3beb7b23b0987dc60814d70e0a88874622d05ed1
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-11-15 12:15:20 -0700

    cs-fixer self fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,30 +1,32 @@
 <?php
 
+declare(strict_types=1);
+
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 return (new Config())
-	->setRiskyAllowed(true)
-	->setRules([
-		'@PSR12' => true,
-		'class_attributes_separation' => true,
-		'ordered_class_elements' => [
-			'sort_algorithm' => 'alpha',
-		],
-		'no_unused_imports' => true,
-		'ordered_imports' => [
-			'imports_order' => ['const', 'class', 'function'],
-		],
-		'not_operator_with_successor_space' => true,
-		'declare_strict_types' => true,
-	])
-	->setFinder(
-		Finder::create()
-			->in(__DIR__)
-			->exclude([
-				'bin',
-				'scratch',
-				'templates',
-				'tests/fixtures',
-			])
-	);
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'class_attributes_separation' => true,
+        'ordered_class_elements' => [
+            'sort_algorithm' => 'alpha',
+        ],
+        'no_unused_imports' => true,
+        'ordered_imports' => [
+            'imports_order' => ['const', 'class', 'function'],
+        ],
+        'not_operator_with_successor_space' => true,
+        'declare_strict_types' => true,
+    ])
+    ->setFinder(
+        Finder::create()
+            ->in(__DIR__)
+            ->exclude([
+                'bin',
+                'scratch',
+                'templates',
+                'tests/fixtures',
+            ])
+    );
