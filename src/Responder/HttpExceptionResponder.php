f247c185e6ae8ff7f4ba0135cf159269a59b904c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2023-11-16 10:51:43 -0700

    Move http exception response prep to repond method

--- src/Responder/HttpExceptionResponder.php
+++ src/Responder/HttpExceptionResponder.php
@@ -8,8 +8,17 @@ use SimpleWpRouting\Exception\HttpExceptionInterface;
 
 final class HttpExceptionResponder extends Responder
 {
+    private HttpExceptionInterface $exception;
+
     public function __construct(HttpExceptionInterface $exception)
     {
-        $exception->prepareResponse($this);
+        $this->exception = $exception;
+    }
+
+    public function respond(): void
+    {
+        $this->exception->prepareResponse($this);
+
+        parent::respond();
     }
 }
